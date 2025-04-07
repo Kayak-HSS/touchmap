309c6a1ecdb10ff420b0f02fbca5753a7cb7b168
Yajat Pathak <yajatpathak16@gmail.com>
2025-04-07 23:04:33 +0530

    added regex

--- src/touchmap/encoder.py
+++ src/touchmap/encoder.py
@@ -17,6 +17,8 @@ def text_to_braille(text, grade: Literal[1, 2] =1, characterError: bool =True, b
             text = str(text)
         else:
             raise TypeError("Unsupported type for text. Must be str, int, float, bool, or implement __str__().")
+    
+    split_text = re.findall(r"\d+\.\d+|[a-zA-Z]+|\d+|[^\w\s]|\s", text)
 
     return ""
 
